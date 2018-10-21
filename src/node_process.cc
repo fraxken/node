5e5684b11cebedafe26bec21766acd0d90ec441d
Romain Lanz <romain.lanz@slynova.ch>
2018-10-21 16:29:52 +0200

    src: refactor deprecated v8::String::NewFromTwoByte call

--- src/node_process.cc
+++ src/node_process.cc
@@ -52,7 +52,9 @@ using v8::HeapStatistics;
 using v8::Integer;
 using v8::Isolate;
 using v8::Local;
+using v8::MaybeLocal;
 using v8::Name;
+using v8::NewStringType;
 using v8::Number;
 using v8::PropertyCallbackInfo;
 using v8::String;
@@ -626,7 +628,8 @@ void EnvGetter(Local<Name> property,
   if ((result > 0 || GetLastError() == ERROR_SUCCESS) &&
       result < arraysize(buffer)) {
     const uint16_t* two_byte_buffer = reinterpret_cast<const uint16_t*>(buffer);
-    Local<String> rc = String::NewFromTwoByte(isolate, two_byte_buffer);
+    MaybeLocal<String> rc = String::NewFromTwoByte(
+        isolate, two_byte_buffer, NewStringType::kNormal);
     return info.GetReturnValue().Set(rc);
   }
 #endif
@@ -768,10 +771,8 @@ void EnvEnumerator(const PropertyCallbackInfo<Array>& info) {
     }
     const uint16_t* two_byte_buffer = reinterpret_cast<const uint16_t*>(p);
     const size_t two_byte_buffer_len = s - p;
-    argv[idx] = String::NewFromTwoByte(isolate,
-                                       two_byte_buffer,
-                                       String::kNormalString,
-                                       two_byte_buffer_len);
+    argv[idx] = String::NewFromTwoByte(
+        isolate, two_byte_buffer, NewStringType::kNormal, two_byte_buffer_len);
     if (++idx >= arraysize(argv)) {
       fn->Call(ctx, envarr, idx, argv).ToLocalChecked();
       idx = 0;
